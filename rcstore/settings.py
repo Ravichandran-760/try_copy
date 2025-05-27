75b31d43f405c93918b7d6b7ecde75936e62db8c
Ravichandran-760 <mravichandran760@gmail.com>
2025-05-27 06:48:53 -0700

    Initial commit

--- rcstore/settings.py
+++ rcstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+48v2!#&&d0418kepukrn0vc%qf0o13w8s6rgz9dp$$c)@7(+h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-render-app-name.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+# Optional: compress static files for better performance
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -131,3 +136,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'app.CustomUser'
+LOGIN_URL = '/login/'
+# LOGIN_REDIRECT_URL = '/profile/'      # After successful login
+# LOGOUT_REDIRECT_URL = '/login/'
